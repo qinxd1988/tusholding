79793b945272fb42252d7aa984fc757a76a91cb9
qinxd <qinxd1988@163.com>
2017-04-25 19:14:41 +0800

    添加ModuleBean

---
+++ app/src/main/java/com/TusFinancial/Credit/bean/ModuleBean.java
@@ -0,0 +1,20 @@
+package com.TusFinancial.Credit.bean;
+
+/**
+ * 作者：qinxudong
+ * <p/>
+ * 时间：2017/4/25 19:10
+ * <p/>
+ * 邮箱：qinxd1988@163.com
+ * <p/>
+ * 描述：
+ */
+public class ModuleBean {
+
+    public String title;
+
+    public String imgUrl;
+
+    public String linkUrl;
+
+}

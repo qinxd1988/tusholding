a6c90b60c30b4858eb11d10baf794db29a2cf62e
qinxd <qinxd1988@163.com>
2017-04-26 12:15:40 +0800

    我的UI完善

--- library/src/main/java/com/base/qinxd/library/utils/ScreenUtils.java
+++ library/src/main/java/com/base/qinxd/library/utils/ScreenUtils.java
@@ -119,4 +119,22 @@ public class ScreenUtils {
 
     }
 
+    /**
+     * dpתpx
+     *
+     */
+    public static int dip2px(Context ctx,float dpValue) {
+        final float scale = ctx.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+
+    /**
+     *	pxתdp
+     */
+    public static int px2dip(Context ctx,float pxValue) {
+        final float scale = ctx.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
 }
